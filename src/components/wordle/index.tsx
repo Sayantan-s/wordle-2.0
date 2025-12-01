5f63543b6e7d9d14b10295b9f909f2e4a3c90777
Sayantan <Sayantan@HyreSnaps-MacBook-Pro.local>
2025-12-01 15:06:07 +0530

    feat: solved the 1st edge case

--- src/components/wordle/index.tsx
+++ src/components/wordle/index.tsx
@@ -33,10 +33,12 @@ const Root: FC<IProps> = ({ attempts, wordSize }) => {
     (letterIndex: number): ChangeEventHandler<HTMLInputElement> =>
     (eve) => {
       const { value } = eve.target;
-      if (!isNaN(Number(value))) return;
+      // if (!isNaN(Number(value))) return;
       setSolutions((prevState) => {
         const clonedSolution = structuredClone(prevState);
-        clonedSolution[activeAttemptIndex][letterIndex] = value;
+        clonedSolution[activeAttemptIndex][letterIndex] = value
+          .slice(-1)
+          .toUpperCase();
         return clonedSolution;
       });
     };
@@ -44,14 +46,51 @@ const Root: FC<IProps> = ({ attempts, wordSize }) => {
   const handleKeyDown =
     (letterIndex: number): React.KeyboardEventHandler<HTMLInputElement> =>
     (eve) => {
+      if (eve.key === "Tab") {
+        eve.preventDefault();
+        inputRefs.current[activeAttemptIndex][letterIndex]?.focus();
+        console.log("stop from moving to right");
+      }
       const currentInputValue = solutions[activeAttemptIndex][letterIndex];
-      console.log(currentInputValue, "currentInputValue");
-      if (currentInputValue.trim() !== "") {
-        if (eve.key === "Enter") {
-          console.log("move to next");
+      if (eve.key === "Enter") {
+        const currentWordArray = solutions[activeAttemptIndex].filter(Boolean);
+        const output = currentWordArray.join("");
+        const isCorrectAnswer = output === WORD;
+        const shouldMoveToNextAttempt =
+          output.length === wordSize && !isCorrectAnswer;
+
+        if (isCorrectAnswer) {
+          console.log("you did it");
         }
+
+        if (shouldMoveToNextAttempt) {
+          setActiveAttemptIndex((prevState) => {
+            const newActiveIndex = prevState + 1;
+            inputRefs.current[newActiveIndex][0]?.focus();
+            return newActiveIndex;
+          });
+        }
+      } else if (eve.key === "Backspace") {
+        if (currentInputValue.trim() === "") {
+          console.log("move to prev", currentInputValue);
+          inputRefs.current[activeAttemptIndex][letterIndex - 1]?.focus();
+        }
+      } else {
+        setTimeout(
+          () => inputRefs.current[activeAttemptIndex][letterIndex + 1]?.focus(),
+          0
+        );
       }
     };
+
+  // const handleBlur =
+  //   (letterIndex: number): React.FocusEventHandler<HTMLInputElement> =>
+  //   () => {
+  //     const currentWordArray = solutions[activeAttemptIndex].filter(Boolean);
+  //     const shouldFocusOnWordIndex = currentWordArray.length;
+  //     console.log(shouldFocusOnWordIndex);
+  //     inputRefs.current[activeAttemptIndex][shouldFocusOnWordIndex]?.focus();
+  //   };
 
   return (
     <div className="wordle__solutions">
@@ -66,6 +105,7 @@ const Root: FC<IProps> = ({ attempts, wordSize }) => {
               key={`${solutionIndex}_${letterIndex}`}
               onChange={handleChangeAttempt(letterIndex)}
               onKeyDown={handleKeyDown(letterIndex)}
+              // onFocus={handleBlur(letterIndex)}
             />
           ))}
         </div>
